dc2cd724f21e5873a4fc03de86bfa50b4b8bad9c
Natalie Gillam <ngillam@hotmail.co.uk>
2024-10-07 01:24:48 +0100

    made Roman numeral converter fully functional

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const convertBtn = document.getElementById("convert-btn");
-
 const numberInput = document.getElementById("number");
 const output = document.getElementById("output");
-// output.style.borderColor = "red";
 
+const romanNumerals = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+let numeralString = "";
 function convertToRoman(input) {
-    let numeralString = "";
-    numeralString += Math.floor(input/1000).toString();
-
-    // M	1000
-    // D	500
-    // C	100
-    // L	50
-    // X	10
-    // V	5
-    // I	1
+  for (let numeral in romanNumerals) {
+    while (input - romanNumerals[numeral] >= 0) {
+      input -= romanNumerals[numeral];
+      numeralString += numeral;
+    }
+  }
+  output.innerText = numeralString;
 }
 
 function processInput() {
+   numeralString = "";
   if (numberInput.value == "") {
     output.innerText = "Please enter a valid number";
   } else if (numberInput.value < 1) {
@@ -30,9 +42,3 @@ function processInput() {
 }
 
 convertBtn.addEventListener("click", processInput);
-
-// When the #number element contains the number 9 and the #convert-btn element is clicked, the #output element should contain the text "IX"
-// When the #number element contains the number 16 and the #convert-btn element is clicked, the #output element should contain the text "XVI"
-// When the #number element contains the number 649 and the #convert-btn element is clicked, the #output element should contain the text "DCXLIX"
-// When the #number element contains the number 1023 and the #convert-btn element is clicked, the #output element should contain the text "MXXIII"
-// When the #number element contains the number 3999 and the #convert-btn element is clicked, the #output element should contain the text "MMMCMXCIX"
